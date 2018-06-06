ad85a8b92f7862e7f81b33fd37b848cc193bfa3d
Emmanuel Guerra <emmanuelali@hotmail.com>
2018-06-05 21:33:07 -0400

    Comentando el código

--- Archivos .ino/LCD.ino
+++ Archivos .ino/LCD.ino
@@ -1,13 +1,17 @@
-​#import <LiquidCrystal.h>
-#include <LiquidCrystal.h>
+/*
+Extraído de https://goo.gl/BxM4zL
+*/
+​#import <LiquidCrystal.h> //En thinkercard hay que importar y luego incluir.
+#include <LiquidCrystal.h> //Hay que revisar si la librería esta en el IDE. Ir a Programa>Incluir libreria>Gestionar librerias y buscar
+  //La librería LiquidCrystal
 
-LiquidCrystal lcd(12,11,5,4,3,2);
+LiquidCrystal lcd(12,11,5,4,3,2);//Los numeros en los que ira conectados al lcd de izquierda a derecha
 
-#define contraste 9
-#define brillo 10
+#define contraste 9 //pin del constrate
+#define brillo 10 //pin del brillo
 
 void setup() {
-  lcd.begin(16,2);
+  lcd.begin(16,2);//Incializar la pantalla con su tamaño
   pinMode(contraste,OUTPUT);
   pinMode(brillo,OUTPUT);
   
@@ -16,7 +20,7 @@ void setup() {
 
 void loop() {
   lcd.print("Hola Mundo!");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,1);//Pone el cursor en la 2 fila y en la columna 0(Recordar que solo tiene 2 y que estas se cuentan desde el 0 como si fuera una matriz en cualquier lenguaje de programacion)
   lcd.print("Pantalla 16x2");
   delay(1000);
   for(int i = 0;i<16;i++){
